14687a4624f9e6b221233b1f251876703c7a6893
nurul <muhammadnurul851@gmail.com>
2026-08-27 13:53:45 +0530

    Spring Application Development using xml+Annotation driven configuration

---
+++ src/com/nt/sbeans1/WishMessageGenerator1.java
@@ -0,0 +1,31 @@
+package com.nt.sbeans1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.time.LocalDate;
+
+@Component("wmg")
+public class WishMessageGenerator {
+    @Autowired  // for field injection
+    private LocalTime lt;
+    @Autowired // for field injection
+    private LocalDate ld;
+
+
+    // b.method
+
+    public String generateWishMessage(String user) {
+        System.out.println("WishMessageGenerator:generateWishMessage:"+lt+".."+ld);
+        // get current hour of the day
+        int hour = lt.getHour(); // 24 hour format
+        if (hour < 12)
+            return "Good Morning:"+ user;
+        else if (hour < 16)
+            return "Good AfterNoon:"+ user;
+        else if (hour < 20)
+            return "Good Evening:"+ user;
+        else
+            return "Good Night:"+ user;
+    }
+}
